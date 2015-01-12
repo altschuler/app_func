861b9234a9ca8026902160a04c9967dfe57717c0
Simon Altschuler <simon@altschuler.dk>
2015-01-12 15:11:54 +0100

    add draw script

---
+++ trees/Draw.fsx
@@ -0,0 +1,22 @@
+#r "../lib/FSharp.PowerPack.dll"
+
+#load "AST.fs"
+#load "Parser.fs"
+#load "Lexer.fs"
+#load "ParserUtil.fs"
+#load "TreeDesign.fs"
+#load "TreeDraw.fs"
+#load "TransformAST.fs"
+
+open AST
+open ParserUtil
+open TreeDesign
+open TreeDraw
+open TransformAST
+
+open System.IO
+
+let ast2ps = drawTree << design << transform
+let file2ps = ast2ps << parseString << File.ReadAllText
+
+File.WriteAllText("tree.ps", file2ps "program/ArrayProg2.while")
