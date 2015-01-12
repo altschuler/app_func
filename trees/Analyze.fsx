e108b6a4a8f718bb817bd28cc6c10074ccf0047f
Simon Altschuler <simon@altschuler.dk>
2015-01-12 14:59:33 +0100

    analyze running time

---
+++ trees/Analyze.fsx
@@ -0,0 +1,41 @@
+#r "../lib/FSharp.PowerPack.dll"
+
+#load "AST.fs"
+#load "Parser.fs"
+#load "Lexer.fs"
+#load "ParserUtil.fs"
+#load "TreeDesign.fs"
+#load "TreeDraw.fs"
+#load "TransformAST.fs"
+
+open AST
+open ParserUtil
+open TreeDesign
+open TreeDraw
+open TransformAST
+
+open System.IO
+open System.Diagnostics
+
+let ast2ps = drawTree << design << transform
+let read file = File.ReadAllText(file)
+let file2ps = ast2ps << parseString << read
+
+let generateProgram size =
+  let decls = List.reduce (+) <| List.replicate size ";int foo : 1"
+  let stms = List.reduce (+) <| List.replicate size ";print \"x\""
+  sprintf "let int foo : 1%s in print \"x\"%s end" decls stms
+
+let time size =
+  let ast = parseString <| generateProgram size
+  let stopwatch = Stopwatch.StartNew()
+  ignore (ast2ps ast)
+  stopwatch.Stop()
+  let ms = stopwatch.Elapsed.TotalMilliseconds
+  ms
+
+// run once to load all modules
+ignore (time 1)
+
+let analyze = List.zip [1 .. 100] (List.map time [1 .. 100])
+printfn "%A" analyze
