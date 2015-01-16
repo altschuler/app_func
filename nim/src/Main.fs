5b69e46cb5fe8aca9197ebe127de5550c2483d30
Markus Faerevaag <m@frv.ag>
2015-01-16 21:16:57 +0100

    parse ui type

--- nim/src/Main.fs
+++ nim/src/Main.fs
@@ -20,28 +20,27 @@ module Main =
 
   // asynchronous event queue kindly provided by Don Syme
   type AsyncEventQueue<'T>() =
-      let mutable cont = None
-      let queue = System.Collections.Generic.Queue<'T>()
-      let tryTrigger () =
-        match queue.Count, cont with
-          | _, None -> ()
-          | 0, _ -> ()
-          | _, Some d ->
-            cont <- None
-            d (queue.Dequeue())
+    let mutable cont = None
+    let queue = System.Collections.Generic.Queue<'T>()
+    let tryTrigger () =
+      match queue.Count, cont with
+        | _, None -> ()
+        | 0, _ -> ()
+        | _, Some d ->
+          cont <- None
+          d (queue.Dequeue())
 
-      let tryListen d =
-        if cont.IsSome then invalidOp "multicast not allowed"
-        cont <- Some d
-        tryTrigger ()
+    let tryListen d =
+      if cont.IsSome then invalidOp "multicast not allowed"
+      cont <- Some d
+      tryTrigger ()
 
-      member x.Post msg = queue.Enqueue msg; tryTrigger()
-      member x.Receive() =
-          Async.FromContinuations (fun (cont,econt,ccont) ->
-              tryListen cont)
+    member x.Post msg = queue.Enqueue msg; tryTrigger()
+    member x.Receive() =
+      Async.FromContinuations (fun (cont,econt,ccont) ->
+          tryListen cont)
 
   // enumeration of the possible events
-
   type Event =
     | Load of string
     | Loaded of string
@@ -56,90 +55,109 @@ module Main =
 
   let ev = AsyncEventQueue()
 
-  let ui =
-    GUI (
-      (fun url  -> ev.Post (Load url)),
-      (fun _    -> ev.Post Cancel),
-      (fun move -> ev.Post (HumanMove move)),
-      (fun _    -> ev.Post ComputerMove)) :> UI
-
   let gameLoader = GameLoader()
 
 
   // functions
-  let tryMove (game:Game) move =
-    try game.Move move
-    with
-      | InvalidMove(s) ->
-        ui.Notify s
-        game
 
-  let rec ready (m : string option) = async {
-    ui.Render (Ready m)
+  let go (ui : UI) =
 
-    let! msg = ev.Receive()
+    let tryMove (game:Game) move =
+      try game.Move move
+      with
+        | InvalidMove(s) ->
+          ui.Notify s
+          game
 
-    match msg with
-      | Load url -> return! loading url
-      | x        -> failwith (sprintf "ready: unexpected message '%A'" x)
-    }
+    let rec ready (m : string option) = async {
+      ui.Render (Ready m)
 
-  and loading url = async {
-    ui.Render (Loading url)
+      let! msg = ev.Receive()
 
-    use ts = new CancellationTokenSource()
-
-    Async.StartWithContinuations (gameLoader.Fetch(url),
-                                  (fun str -> ev.Post (Loaded str)),
-                                  (fun _ -> ev.Post Error),
-                                  (fun _ -> ev.Post Cancelled),
-                                  ts.Token)
-
-    let! msg = ev.Receive()
-
-    try
       match msg with
-        | Loaded str ->
-          try
-            let game = new Game(Human, new Board(parseInts str), false)
-            return! play game None
-          with
-            | :? System.FormatException -> raiseParseError url
-        | Error   -> return! ready (Some "An error occured while fetching")
-        | Cancel  ->
-          ts.Cancel()
-          return! cancelling ()
-        | x       -> failwith (sprintf "loading: unexpected message '%A'" x)
-    with
-      | ParseError(s) -> return! ready (Some s)
-    }
+        | Load url -> return! loading url
+        | x        -> failwith (sprintf "ready: unexpected message '%A'" x)
+      }
 
-  and cancelling () = async {
-    ui.Render Cancelling
+    and loading url = async {
+      ui.Render (Loading url)
 
-    let! msg = ev.Receive()
+      use ts = new CancellationTokenSource()
 
-    match msg with
-      | Cancelled
-      | Error
-      | Loaded _ -> return! ready (Some "Cancelled fetch")
-      | x        -> failwith (sprintf "cancelling: unexpected message '%A'" x)
-    }
+      Async.StartWithContinuations (gameLoader.Fetch(url),
+                                    (fun str -> ev.Post (Loaded str)),
+                                    (fun _ -> ev.Post Error),
+                                    (fun _ -> ev.Post Cancelled),
+                                    ts.Token)
 
-  and play (game:Game) ss = async {
-    ui.Render (Playing (game, ss))
+      let! msg = ev.Receive()
 
-    let! msg = ev.Receive()
+      try
+        match msg with
+          | Loaded str ->
+            try
+              let game = new Game(Human, new Board(parseInts str), false)
+              return! play game None
+            with
+              | :? System.FormatException -> raiseParseError url
+          | Error   -> return! ready (Some "An error occured while fetching")
+          | Cancel  ->
+            ts.Cancel()
+            return! cancelling ()
+          | x       -> failwith (sprintf "loading: unexpected message '%A'" x)
+      with
+        | ParseError(s) -> return! ready (Some s)
+      }
 
-    match msg with
-      | Cancel       -> return! ready None
-      | HumanMove m  -> return! play (tryMove game m) None
-      | ComputerMove -> return! play (game.ComputerMove()) None // TODO: fix ai
-      | x            -> failwith (sprintf "play: unexpected message '%A'" x)
-    }
+    and cancelling () = async {
+      ui.Render Cancelling
+
+      let! msg = ev.Receive()
+
+      match msg with
+        | Cancelled
+        | Error
+        | Loaded _ -> return! ready (Some "Cancelled fetch")
+        | x        -> failwith (sprintf "cancelling: unexpected message '%A'" x)
+      }
+
+    and play (game:Game) ss = async {
+      ui.Render (Playing (game, ss))
+
+      let! msg = ev.Receive()
+
+      match msg with
+        | Cancel       -> return! ready None
+        | HumanMove m  -> return! play (tryMove game m) None
+        | ComputerMove -> return! play (game.ComputerMove()) None // TODO: fix ai
+        | x            -> failwith (sprintf "play: unexpected message '%A'" x)
+      }
 
 
-  // go
+    // go
 
-  Async.StartImmediate(ready None)
-  ui.Go ()
+    Async.StartImmediate(ready None)
+    ui.Go ()
+
+
+  [<EntryPoint>]
+  let main args =
+
+    // cli
+    if Array.exists ((=) "-nw") args
+    then
+      printfn "TODO CLI"
+
+    // gui
+    else
+      let ui =
+        GUI (
+          (fun url  -> ev.Post (Load url)),
+          (fun _    -> ev.Post Cancel),
+          (fun move -> ev.Post (HumanMove move)),
+          (fun _    -> ev.Post ComputerMove)
+          ) :> UI
+
+      go ui
+
+    0
