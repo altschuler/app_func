018aa7fc3fe2c4389041c9896d93db1990941cc2
Simon Altschuler <simon@altschuler.dk>
2015-01-20 13:10:44 +0100

    simplified main

--- nim/src/Main.fs
+++ nim/src/Main.fs
@@ -10,21 +10,10 @@ module Main =
   open Nim.CLI
 
   let parseUI args =
+    let fns = (loadFn, cancelFn, moveFn, compFn)
     if Array.exists ((=) "-nw") args
-    then
-      CLI (
-        (loadFn),
-        (cancelFn),
-        (moveFn),
-        (compFn)
-        ) :> UI
-    else
-      GUI (
-        (loadFn),
-        (cancelFn),
-        (moveFn),
-        (compFn)
-        ) :> UI
+    then CLI fns :> UI
+    else GUI fns :> UI
 
   [<EntryPoint>]
   let main args =
